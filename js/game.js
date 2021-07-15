bf3b6292294bd58d6399050aef3ae1222f3c456c
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2021-07-16 00:53:14 +0700

    Update game.js

--- js/game.js
+++ js/game.js
@@ -7,6 +7,9 @@ rRadius = 0;
 ch = -3;
 cs = 0;
 Data = [];
+bfs = [];
+auto = false;
+index = 1;
 
 var bg_im = new Image();
 bg_im.src = "images/bg.png";
@@ -50,6 +53,8 @@ class game {
 
     listenMouse() {
         document.addEventListener("mousedown", evt => {
+            if (auto)
+                return;
             var x = evt.offsetX == undefined ? evt.layerX : evt.offsetX;
             var y = evt.offsetY == undefined ? evt.layerY : evt.offsetY;
             if (x >= xTT && x <= xTT + size && y >= yTT && y <= yTT + size) {
@@ -58,10 +63,14 @@ class game {
                 if (Data[XX][YY] != 0) {
                     xStart = XX;
                     yStart = YY;
-                } else {
+                } else if (xStart != -1) {
                     xEnd = XX;
                     yEnd = YY;
-                    this.solve(xStart, yStart, xEnd, yEnd);
+                    bfs = this.solve(xStart, yStart, xEnd, yEnd);
+                    if (bfs.length > 0) {
+                        auto = true;
+                        index = 1;
+                    }
                     xStart = yStart = xEnd = yEnd = -1;
                 }
             }
@@ -79,7 +88,40 @@ class game {
     }
 
     solve(xStart, yStart, xEnd, yEnd) {
-        console.log(xStart, ' ', yStart, ' ', xEnd, ' ', yEnd);
+        let t = [];
+        for (let i = 0; i < 9; i++)
+            t[i] = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+        let queue = [{x : xStart, y : yStart}];
+        let index = 0;
+        while (index < queue.length) {
+            let X = queue[index].x;
+            let Y = queue[index++].y;
+            for (let i = X - 1; i <= X + 1; i++)
+                for (let j = Y - 1; j <= Y + 1; j++)
+                    if (this.isPoint(i, j) && t[i][j] == 0 && Math.abs(i - X) + Math.abs(j - Y) == 1 && Data[i][j] == 0) {
+                        queue.push({x : i, y : j});
+                        t[i][j] = {x : X, y : Y};
+                    }
+        }
+        let ans = [];
+        if (t[xEnd][yEnd] != 0) {
+            ans.push({x : xEnd, y : yEnd});
+            let X = xEnd;
+            let Y = yEnd;
+            while (X != xStart || Y != yStart) {
+                let Xt = t[X][Y].x;
+                Y = t[X][Y].y;
+                X = Xt;
+                ans.push({x : X, y : Y});
+            }
+        }
+        return ans.reverse();
+    }
+
+    isPoint(x, y) {
+        if (x < 0 || x > 8 || y < 0 || y > 8)
+            return false;
+        return true;
     }
 
 
@@ -94,7 +136,13 @@ class game {
         if (rRadius < (1 - 2/7) * (size / 9) / 2 || rRadius > (1 - 2/7) * (size / 9))
             ch *= -1;
         rRadius += ch;
-        // console.log(rRadius, ' ', (1 - 2/7) * (size / 9));
+        if (auto) {
+            Data[bfs[index].x][bfs[index].y] = Data[bfs[index - 1].x][bfs[index - 1].y];
+            Data[bfs[index - 1].x][bfs[index - 1].y] = 0;
+            index++;
+            if (index >= bfs.length)
+                auto = false;
+        }
     }
 
  
